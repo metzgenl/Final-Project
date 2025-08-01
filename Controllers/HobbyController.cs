7ddb4f01031dbec7cb4e2d6650d4f782ea51a5c0
Rudi <vogelrj@mail.uc.edu>
2025-08-01 17:34:08 -0400

    added hoppy controller

---
+++ Controllers/HobbyController.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using Final_Project.Data;
+using Final_Project.Models;
+ 
+namespace Final_Project.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HobbyController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+ 
+        public HobbyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+ 
+        // GET: api/Hobby
+        [HttpGet]
+        public IActionResult GetHobbies()
+        {
+            return Ok(_context.Hobbies.Take(5).ToList());
+        }
+ 
+        // GET: api/Hobby/5
+        [HttpGet("{id}")]
+        public IActionResult GetHobby(int id)
+        {
+            var hobby = _context.Hobbies.Find(id);
+            if (hobby == null) return NotFound();
+            return Ok(hobby);
+        }
+ 
+        // POST: api/Hobby
+        [HttpPost]
+        public IActionResult CreateHobby([FromBody] Hobby hobby)
+        {
+            _context.Hobbies.Add(hobby);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetHobby), new { ID = hobby.ID }, hobby);
+        }
+ 
+        // PUT: api/Hobby/5
+        [HttpPut("{ID}")]
+        public IActionResult UpdateHobby(int id, [FromBody] Hobby updatedHobby)
+        {
+            var hobby = _context.Hobbies.Find(id);
+            if (hobby == null) return NotFound();
+ 
+            hobby.HobbyName = updatedHobby.HobbyName;
+            hobby.Description = updatedHobby.Description;
+            hobby.SkillLevel = updatedHobby.SkillLevel;
+ 
+            _context.SaveChanges();
+            return NoContent();
+        }
+ 
+        // DELETE: api/Hobby/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteHobby(int id)
+        {
+            var hobby = _context.Hobbies.Find(id);
+            if (hobby == null) return NotFound();
+ 
+            _context.Hobbies.Remove(hobby);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
